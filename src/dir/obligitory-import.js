0904ac5b39096f1d13fbfa1303bfd34f8cd1f072
mwj8410 <mjackson@treehouseonline.com>
2017-07-29 18:26:29 -0500

    chore(import) demonstrate multiple value import

--- src/dir/obligitory-import.js
+++ src/dir/obligitory-import.js
@@ -1,1 +1,3 @@
 export const test = 'From Import';
+
+export const test2 = 'Also From Import';
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
-import { test } from 'Dir/obligitory-import';
+import { test, test2 } from 'Dir/obligitory-import';
 
 console.log(test);
+
+console.log(test2);
